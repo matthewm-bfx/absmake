0001b8e35902e9a7f70e7edfbeb70c0fa359b63c
Matthew Mott <matthewm@borisfx.com>
2022-10-19 17:22:31 +0100

    Interpret "leaving directory" line

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 use std::env;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::path::PathBuf;
 use std::process::Command;
 use std::process::Stdio;
 
@@ -21,9 +22,19 @@ impl DirectoryTracker {
         let enter_re = Regex::new(r"^make\[[1-9]\]: Entering directory '([^']+)'").unwrap();
         let leave_re = Regex::new(r"^make\[[1-9]\]: Leaving directory '([^']+)'").unwrap();
 
+        // Handle entering a directory
         if let Some(caps) = enter_re.captures(line) {
             self.current_dir = caps.get(1).unwrap().as_str().to_owned();
             println!("+ Current dir is now {}", self.current_dir);
+        }
+        // Handle leaving a directory. We can only leave a directory if we are already in it.
+        else if let Some(caps) = leave_re.captures(line) {
+            let left_dir = caps.get(1).unwrap().as_str().to_owned();
+            if left_dir == self.current_dir {
+                let path = PathBuf::from(left_dir);
+                let parent = path.parent().expect("failed to identify parent path");
+                self.current_dir = parent.to_str().unwrap().to_owned();
+            }
         }
     }
 }
@@ -75,8 +86,14 @@ mod tests {
         tracker.interpret_line("random line");
         assert_eq!(tracker.current_dir, "");
 
-        let line = r"make[1]: Entering directory '/home/me/source/main'";
-        tracker.interpret_line(line);
+        // Enter directory
+        let enter_line = r"make[1]: Entering directory '/home/me/source/main'";
+        tracker.interpret_line(enter_line);
         assert_eq!(tracker.current_dir, "/home/me/source/main");
+
+        // Leave directory
+        let leave_line = r"make[1]: Leaving directory '/home/me/source/main'";
+        tracker.interpret_line(leave_line);
+        assert_eq!(tracker.current_dir, "/home/me/source");
     }
 }
